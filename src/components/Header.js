b9fbd1d608c60e043e3f77e2c7b441e3e5c9fcdf
Pathan-Riyaz-Khan <pathanriyazkhan136@gmail.com>
2026-07-01 08:11:31 +0530

    sign/ singup form && login and header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 z-10">
+      <img className="w-40 h-20" src="Netflix_Logo_RGB.png" alt="Netflix" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Header from "./Header";
+import { useState } from "react";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/81b52f88-dc76-488d-a939-0cf13a260a6e/web/IN-en-20260622-TRIFECTA-perspective_d39d60ef-cb5a-4793-9546-0a8d9a87948e_medium.jpg"
+          alt="Netflix"
+        />
+      </div>
+      <form className="absolute w-3/12 my-36 mx-auto right-0 left-0 p-12 bg-black text-white bg-opacity-75">
+        <h1 className="text-3xl font-bold py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className=" p-4 my-6 bg-red-700 w-full" type="submit">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already have an account? Sign In"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
